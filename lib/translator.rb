1f22dc1edcfac9c299bfdef88649af4de9589cf8
Adam Shilling <admshng@gmail.com>
2020-09-22 18:10:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,7 +27,7 @@ end
 def get_japanese_emoticon(path, emoticon)
     load_library(path).each do |key, value| 
       if value[:english] == emoticon 
-        return value[:japenese]
+        return value[:japanese]
     end 
   end
 end
